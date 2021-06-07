1a86c383f9bdde758065d5a5cc55b15fd48bb830
Ivan Montoya <moty5871@gmail.com>
2021-06-07 00:56:08 -0500

    Reestructurando el proyecto

--- HuertoMamey/index.php
+++ HuertoMamey/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once("../app/inicio.php");
+require_once("app/inicio.php");
 $control = new Control();
 if ($_SERVER['REQUEST_METHOD']=="POST") {
       //Recibimos la información PHP7 isset()?valor1:valor2 => valor1 ?? valor2
